032673c02c5ca6f4f919bc0f1d8c9fb99f58cb38
wufy1992 <wufangyu1992@gmail.com>
2021-04-03 18:33:44 +0800

    【性能测试】增加logging性能测试对比

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
+import os
 import time
+import logging
 from fast_log import FastLog
 
 
@@ -22,8 +24,25 @@ def test_performance(fast_log):
         time.sleep(0.01)
 
 
+@calc_time
+def test_logging__performance():
+    for i in range(100000):
+        logging.log(logging.INFO, 'test log write performance')
+
+
 if __name__ == "__main__":
     test_fast_log = FastLog()
+
     # test_performance calc time: 2.0698201656341553
     test_performance(test_fast_log)
+    # test clear
     test_fast_log.clear_old_log(3, 100)
+
+    # test_logging__performance calc time: 6.841032981872559
+    test_logging_path = 'logs//test_logging.log'
+    if os.path.exists(test_logging_path):
+        os.remove(test_logging_path)
+    logging.basicConfig(filename=test_logging_path,
+                        format='[%(asctime)s-%(levelname)s:%(message)s]', level=logging.INFO,
+                        filemode='a+', datefmt='%Y-%m-%d%I:%M:%S %p')
+    test_logging__performance()
